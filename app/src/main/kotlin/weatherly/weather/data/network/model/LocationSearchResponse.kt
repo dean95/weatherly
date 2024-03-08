330fcc8e6e816508e8b2c15ad44176b9c67d58c3
Dean Djermanovic <dean.djermanovic@five.agency>
2024-03-08 15:29:18 +0100

    Add LocationSearchResponse model

---
+++ app/src/main/kotlin/weatherly/weather/data/network/model/LocationSearchResponse.kt
@@ -0,0 +1,39 @@
+package weatherly.weather.data.network.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LocationSearchResultItem(
+    @SerialName("AdministrativeArea")
+    val administrativeArea: AdministrativeArea,
+    @SerialName("Country")
+    val country: Country,
+    @SerialName("Key")
+    val key: String,
+    @SerialName("LocalizedName")
+    val localizedName: String,
+    @SerialName("Rank")
+    val rank: Int,
+    @SerialName("Type")
+    val type: String,
+    @SerialName("Version")
+    val version: Int
+) {
+
+    @Serializable
+    data class Country(
+        @SerialName("ID")
+        val id: String,
+        @SerialName("LocalizedName")
+        val localizedName: String
+    )
+
+    @Serializable
+    data class AdministrativeArea(
+        @SerialName("ID")
+        val id: String,
+        @SerialName("LocalizedName")
+        val localizedName: String
+    )
+}
