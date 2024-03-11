abcfa7f733930de955b292cc4701fd1f34f4fd89
Dean Djermanovic <dean.djermanovic@five.agency>
2024-03-11 11:43:01 +0100

    Add fetchFiveDayForecast API endpoint

--- app/src/main/kotlin/weatherly/weather/data/network/api/WeatherApi.kt
+++ app/src/main/kotlin/weatherly/weather/data/network/api/WeatherApi.kt
@@ -1,8 +1,11 @@
 package weatherly.weather.data.network.api
 
+import weatherly.weather.data.network.model.DailyForecastResponse
 import weatherly.weather.data.network.model.LocationSearchResultItem
 
 interface WeatherApi {
 
     suspend fun searchLocation(query: String): List<LocationSearchResultItem>
+
+    suspend fun fetchFiveDayForecast(locationKey: String, metric: Boolean = true): DailyForecastResponse
 }
--- app/src/main/kotlin/weatherly/weather/data/network/api/WeatherApiImpl.kt
+++ app/src/main/kotlin/weatherly/weather/data/network/api/WeatherApiImpl.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import weatherly.core.api.BaseApi
+import weatherly.weather.data.network.model.DailyForecastResponse
 import weatherly.weather.data.network.model.LocationSearchResultItem
 
 private const val BASE_URL = "http://dataservice.accuweather.com"
@@ -15,5 +16,9 @@ internal class WeatherApiImpl(
 
     override suspend fun searchLocation(query: String): List<LocationSearchResultItem> = execute {
         get("$BASE_URL/locations/v1/cities/autocomplete?apikey=$API_KEY&q=$query").body()
+    }
+
+    override suspend fun fetchFiveDayForecast(locationKey: String, metric: Boolean): DailyForecastResponse = execute {
+        get("$BASE_URL/forecasts/v1/daily/5day/$locationKey?apikey=$API_KEY&metric=$metric").body()
     }
 }
